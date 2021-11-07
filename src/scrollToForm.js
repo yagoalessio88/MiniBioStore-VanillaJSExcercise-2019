5ed61abff7d3f6ff5d48a40bdfa204142262cd12
yagoalessio88 <yagoalessio88@gmail.com>
2021-11-07 20:56:40 +0100

    scrollToForm positioning fix

--- src/scrollToForm.js
+++ src/scrollToForm.js
@@ -4,7 +4,7 @@ scrollLink.addEventListener("click", (e) => {
   // prevent default
   e.preventDefault();
   const formSection = document.querySelector(".form-center");
-  let position = formSection.offsetTop - 40;
+  let position = formSection.offsetTop - 48;
   window.scrollTo({
     left: 0,
     // top: element.offsetTop,
