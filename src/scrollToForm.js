5e8bdc07f7e351f7da6e558ed7c0f59736ab47c7
yagoalessio88 <yagoalessio88@gmail.com>
2021-11-07 21:02:51 +0100

    scrollToForm fix 2

--- src/scrollToForm.js
+++ src/scrollToForm.js
@@ -3,8 +3,9 @@ const scrollLink = document.querySelector(".procesar");
 scrollLink.addEventListener("click", (e) => {
   // prevent default
   e.preventDefault();
+  const navbarHeight = document.querySelector(".navbar").clientHeight;
   const formSection = document.querySelector(".form-center");
-  let position = formSection.offsetTop - 48;
+  let position = formSection.offsetTop - navbarHeight;
   window.scrollTo({
     left: 0,
     // top: element.offsetTop,
