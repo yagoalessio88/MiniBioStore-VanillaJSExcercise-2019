b00a06dfc8970eab90aa54bc27831fcfcc96bbbb
yagoalessio88 <yagoalessio88@gmail.com>
2021-11-07 20:16:46 +0100

    js addProduct bug fix quantity layer

--- src/addProductDrag.js
+++ src/addProductDrag.js
@@ -6,16 +6,19 @@ const addProductDrag = (id) => {
     const quantity = quantityLayer.querySelector(".quantity").value;
     addProduct(id, quantity);
     quantityLayer.classList.add("display-none");
+    storeSection.classList.remove("display-none");
     navBar.classList.remove("display-none");
     productsSection.classList.remove("display-none");
     btn.removeEventListener("click", agregar);
   };
 
-  const productsSection = get(".products-section");
-  console.log(productsSection);
+  // const productsSection = get(".products-section");
   const quantityLayer = get(".product-quantity-desktop");
+  const storeSection = get(".store");
+  console.log(storeSection);
   const navBar = get(".navbar");
   quantityLayer.classList.remove("display-none");
+  storeSection.classList.add("display-none");
   navBar.classList.add("display-none");
   //productsSection.classList.add("display-none");
   const btn = quantityLayer.querySelector(".quantity-layer-btn");
