2c62e1c15fadfd8fe9b280235bcc4b3e91afb274
yagoalessio88 <yagoalessio88@gmail.com>
2021-11-07 20:51:41 +0100

    remove eventListener bug fix

--- src/addProductDrag.js
+++ src/addProductDrag.js
@@ -8,21 +8,18 @@ const addProductDrag = (id) => {
     quantityLayer.classList.add("display-none");
     storeSection.classList.remove("display-none");
     navBar.classList.remove("display-none");
-    productsSection.classList.remove("display-none");
     btn.removeEventListener("click", agregar);
   };
 
-  // const productsSection = get(".products-section");
   const quantityLayer = get(".product-quantity-desktop");
   const storeSection = get(".store");
-  console.log(storeSection);
   const navBar = get(".navbar");
   quantityLayer.classList.remove("display-none");
   storeSection.classList.add("display-none");
   navBar.classList.add("display-none");
-  //productsSection.classList.add("display-none");
   const btn = quantityLayer.querySelector(".quantity-layer-btn");
   btn.addEventListener("click", agregar);
+  console.log(btn);
 };
 
 export default addProductDrag;
